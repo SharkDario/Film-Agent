1b7ae3177a1104d1ff643ba7f17eda2970b6dbed
Marianela Acosta <acostagm6@gmail.com>
2026-04-05 14:21:37 -0300

    a

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,20 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'image.tmdb.org',
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+      {
+        protocol: "https",
+        hostname: "i.blogs.es",
+      },
+      {
+        protocol: "https",
+        hostname: "www.espinof.com",
       },
     ],
   },
